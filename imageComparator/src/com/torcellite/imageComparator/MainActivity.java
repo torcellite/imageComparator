2a9707f992726eea1f65dc65ede9fba2a8a4b4c0
torcellite <torcellite@gmail.com>
2013-03-24 17:31:22 +0530

    Initial project
    v1.0

---
+++ imageComparator/src/com/torcellite/imageComparator/MainActivity.java
@@ -0,0 +1,125 @@
+package com.torcellite.imageComparator;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.highgui.Highgui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "OCVSample::Activity";
+	Mat img1, img2, descriptors, dupDescriptors;
+	FeatureDetector detector;
+	DescriptorExtractor SurfExtractor;
+	DescriptorMatcher matcher;
+	MatOfKeyPoint keypoints, dupKeypoints;
+	MatOfDMatch matches;
+	TextView tv;
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+
+				compare();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
+	/*
+	 * Compares two images and states if they're duplicate or not. Keypoints are
+	 * detected and descriptors are extracted and compared. The algorithm - If
+	 * the matches is 15% less than or equal to the duplicate descriptors or
+	 * actual descriptors, the images are recognized as duplicates. People are
+	 * welcome to change the algorithm.
+	 */
+	void compare() {
+		img1 = Highgui.imread("mnt/sdcard/img1.jpg");
+		img2 = Highgui.imread("mnt/sdcard/img2.jpg");
+		detector = FeatureDetector.create(FeatureDetector.FAST);
+		SurfExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+		matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
+
+		keypoints = new MatOfKeyPoint();
+		dupKeypoints = new MatOfKeyPoint();
+		descriptors = new Mat();
+		dupDescriptors = new Mat();
+		matches = new MatOfDMatch();
+		detector.detect(img1, keypoints);
+		Log.d("LOG!", "number of query Keypoints= " + keypoints.size());
+		detector.detect(img2, dupKeypoints);
+		Log.d("LOG!", "number of dup Keypoints= " + dupKeypoints.size());
+		// Descript keypoints
+		SurfExtractor.compute(img1, keypoints, descriptors);
+		SurfExtractor.compute(img2, dupKeypoints, dupDescriptors);
+		Log.d("LOG!", "number of descriptors= " + descriptors.size());
+		Log.d("LOG!", "number of dupDescriptors= " + dupDescriptors.size());
+		// matching descriptors
+		matcher.match(descriptors, dupDescriptors, matches);
+		Log.d("LOG!", "Matches Size " + matches.size());
+		tv.setText("Number of matches:" + matches.size());
+		int m = 0, d = 0, dd = 0, pos = 0;
+		String M, D, DD;
+		D = descriptors.size().toString();
+		pos = D.indexOf('x');
+		d = Integer.parseInt(D.substring(pos + 1));
+		DD = dupDescriptors.size().toString();
+		pos = DD.indexOf('x');
+		dd = Integer.parseInt(DD.substring(pos + 1));
+		M = matches.size().toString();
+		pos = M.indexOf('x');
+		m = Integer.parseInt(M.substring(pos + 1));
+		System.out.println("dd=" + dd + "d=" + d + "m=" + m);
+		if (d > dd) {
+			if (((m - dd) / dd <= .15 && (m - d) / d <= .15)
+					&& (d - dd) / d <= 50) {
+				tv.setText("Duplicate image.");
+			} else
+				tv.setText("Not duplicate images.");
+		} else {
+			if (((m - dd) / dd <= .15 && (m - d) / d <= .15)
+					&& (d - dd) / dd <= 50) {
+				tv.setText("Duplicate image.");
+			} else
+				tv.setText("Not duplicate images.");
+		}
+	}
+
+	public MainActivity() {
+		Log.i(TAG, "Instantiated new " + this.getClass());
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(TAG, "called onCreate");
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+		tv = (TextView) findViewById(R.id.tv);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this,
+				mLoaderCallback);
+	}
+}
