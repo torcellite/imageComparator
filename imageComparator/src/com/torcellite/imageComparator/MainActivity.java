b88ab7b2dd42871a132acf15cb95e389c5e9d37e
torcellite <torcellite@gmail.com>
2013-08-11 20:46:33 +0530

    Notification added
    Added notification for easy access

--- imageComparator/src/com/torcellite/imageComparator/MainActivity.java
+++ imageComparator/src/com/torcellite/imageComparator/MainActivity.java
@@ -180,20 +180,20 @@ public class MainActivity extends Activity {
 					Utils.bitmapToMat(bmpimg1, img1);
 			        Mat img2 = new Mat();
 			        Utils.bitmapToMat(bmpimg2, img2);
-			        //Imgproc.cvtColor(img1, img1, Imgproc.COLOR_RGBA2GRAY); 
-			        //Imgproc.cvtColor(img2, img2, Imgproc.COLOR_RGBA2GRAY); 
+			        Imgproc.cvtColor(img1, img1, Imgproc.COLOR_RGBA2GRAY); 
+			        Imgproc.cvtColor(img2, img2, Imgproc.COLOR_RGBA2GRAY); 
 			        img1.convertTo(img1, CvType.CV_32F);
 			        img2.convertTo(img2, CvType.CV_32F);
-			        Log.d("ImageComparator", "img1:"+img1.rows()+"x"+img1.cols()+" img2:"+img2.rows()+"x"+img2.cols());
+			        //Log.d("ImageComparator", "img1:"+img1.rows()+"x"+img1.cols()+" img2:"+img2.rows()+"x"+img2.cols());
 			        Mat hist1 = new Mat();
 			        Mat hist2 = new Mat();
-			        MatOfInt histSize = new MatOfInt(255);
+			        MatOfInt histSize = new MatOfInt(180);
 			        MatOfInt channels = new MatOfInt(0);
 			        ArrayList<Mat> bgr_planes1= new ArrayList<Mat>();
 			        ArrayList<Mat> bgr_planes2= new ArrayList<Mat>();
 			        Core.split(img1, bgr_planes1);
 			        Core.split(img2, bgr_planes2);
-			        MatOfFloat histRanges = new MatOfFloat (0f, 255f);		        
+			        MatOfFloat histRanges = new MatOfFloat (0f, 180f);		        
 			        boolean accumulate = false;
 			        Imgproc.calcHist(bgr_planes1, channels, new Mat(), hist1, histSize, histRanges, accumulate);
 		        	Core.normalize(hist1, hist1, 0, hist1.rows(), Core.NORM_MINMAX, -1, new Mat());
@@ -204,12 +204,13 @@ public class MainActivity extends Activity {
 				        hist1.convertTo(hist1, CvType.CV_32F);
 				        hist2.convertTo(hist2, CvType.CV_32F);
 				
-			        	double compare = Imgproc.compareHist(hist1, hist2, Imgproc.CV_COMP_CORREL);
-			        	if(compare<1 && compare>0.9) {
+			        	double compare = Imgproc.compareHist(hist1, hist2, Imgproc.CV_COMP_CHISQR);
+			        	Log.d("ImageComparator", "compare: "+compare);
+			        	if(compare>0 && compare<1500) {
 			        		Toast.makeText(MainActivity.this, "Images may be possible duplicates, verifying", Toast.LENGTH_LONG).show();
 			        		new asyncTask(MainActivity.this).execute();
 			        	}
-			        	else if(compare==1)
+			        	else if(compare==0)
 			        		Toast.makeText(MainActivity.this, "Images are exact duplicates", Toast.LENGTH_LONG).show();
 			        	else
 			        		Toast.makeText(MainActivity.this, "Images are not duplicates", Toast.LENGTH_LONG).show();
